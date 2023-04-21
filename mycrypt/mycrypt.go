c2c8bb10458c4982f2bfc764ab56b2bf5c5831eb
NorwegianKiwi_glitch <simonhelgen1@gmail.com>
2023-04-21 09:46:39 +0200

    bravo

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -2,10 +2,26 @@ package mycrypt
 
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; KSN")
 
+/*
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
+		shiftedIndex := (indeks + chiffer) % len(alphabet)
+		kryptertMelding[i] = alphabet[shiftedIndex]
+	}
+	return kryptertMelding
+}*/
+
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+	kryptertMelding := make([]rune, len(melding))
+	for i := 0; i < len(melding); i++ {
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		if indeks < 0 {
+			// symbol not found in the alphabet, skip encryption
+			kryptertMelding[i] = melding[i]
+			continue
+		}
 		shiftedIndex := (indeks + chiffer) % len(alphabet)
 		kryptertMelding[i] = alphabet[shiftedIndex]
 	}
